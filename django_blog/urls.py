2bfd7e7c9a3426d6342b5b7165c597692396c632
StevenWang <wangzhou8284@gmail.com>
2018-04-13 10:01:23 +0800

    add feature:avatar

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
                   path('account/', include('django.contrib.auth.urls')),
                   path('blog/', include('blog.urls')),
                   path('admin/', admin.site.urls),
-                  path(r'markdownx/', include('markdownx.urls')), ] \
+                  path(r'markdownx/', include('markdownx.urls')),
+                  path(r'avatar/', include('avatar.urls'), )] \
               + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
